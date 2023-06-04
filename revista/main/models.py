ee9219883f781960e55ed77ed43a7e0c6e1bd027
reham-shaban <reham.shaban2002@gmail.com>
2023-06-04 13:26:07 -0400

    edit images path

--- revista/main/models.py
+++ revista/main/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    cover_image = models.ImageField(blank=True, null=True)
+    cover_image = models.ImageField(upload_to='cover_images/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
