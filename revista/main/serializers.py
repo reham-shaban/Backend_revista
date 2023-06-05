6002a3401b4658d13883a2305276be7a5d051977
reham-shaban <reham.shaban2002@gmail.com>
2023-06-05 13:34:23 -0400

    .

--- revista/main/serializers.py
+++ revista/main/serializers.py
@@ -9,11 +9,24 @@ class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     followers_count = serializers.SerializerMethodField(method_name='get_followers_count')
     following_count = serializers.SerializerMethodField(method_name='get_following_count')
+    posts_count = serializers.SerializerMethodField(method_name='get_posts_count')
        
     class Meta:
         model = Profile
-        fields = ('id', 'user', 'cover_image', 'bio', 'followers_count', 'following_count', 'created_at', 'updated_at')
-        read_only_fields = ['id', 'user', 'followers_count', 'following_count', 'created_at', 'updated_at']
+        fields = ('id', 'user', 'cover_image', 'bio', 'followers_count', 'following_count', 'posts_count', 'created_at', 'updated_at')
+        read_only_fields = ['id', 'user', 'followers_count', 'following_count', 'posts_count', 'created_at', 'updated_at']
+          
+    # def update(self, instance, validated_data):
+    #     user_data = validated_data.pop('user', None)
+    #     if user_data:
+    #         user_serializer = UserSerializer(instance.user, data=user_data, partial=True)
+    #         if user_serializer.is_valid():
+    #             user_serializer.save()
+        
+    #     instance.__dict__.update(**validated_data)
+    #     instance.save()
+    #     return instance
+
           
     def get_followers_count(self, obj):
         followers_count = Follow.objects.filter(followed=obj.id).count()
@@ -22,18 +35,22 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_following_count(self, obj):
         following_count = Follow.objects.filter(follower=obj.id).count()
         return following_count
+    
+    def get_posts_count(self, obj):
+        return 0
  
 # same above but has is_following field      
 class VistorProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     followers_count = serializers.SerializerMethodField(method_name='get_followers_count')
     following_count = serializers.SerializerMethodField(method_name='get_following_count')
+    posts_count = serializers.SerializerMethodField(method_name='get_posts_count')
     is_following = serializers.SerializerMethodField(method_name='check_following')
    
     class Meta:
         model = Profile
-        fields = ('id', 'user', 'cover_image', 'bio', 'is_following', 'followers_count', 'following_count', 'created_at', 'updated_at')
-        read_only_fields = ['id', 'user', 'is_following', 'followers_count', 'following_count', 'created_at', 'updated_at']
+        fields = ('id', 'user', 'cover_image', 'bio', 'is_following', 'followers_count','posts_count', 'following_count', 'created_at', 'updated_at')
+        read_only_fields = ['id', 'user', 'is_following', 'followers_count', 'following_count', 'posts_count', 'created_at', 'updated_at']
           
     def get_followers_count(self, obj):
         followers_count = Follow.objects.filter(followed=obj.id).count()
@@ -42,6 +59,9 @@ class VistorProfileSerializer(serializers.ModelSerializer):
     def get_following_count(self, obj):
         following_count = Follow.objects.filter(follower=obj.id).count()
         return following_count
+    
+    def get_posts_count(self, obj):
+        return 0
     
     def check_following(self, obj):
         request = self.context.get('request')
@@ -89,11 +109,11 @@ class FollowingListSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Follow
-        fields = ['id', 'followed']
+        fields = ['followed']
         
 class FollowersListSerializer(serializers.ModelSerializer):
     follower = ProfileFollowListSerializer(read_only=True)
     
     class Meta:
         model = Follow
-        fields = ['id', 'follower']
+        fields = ['follower']
