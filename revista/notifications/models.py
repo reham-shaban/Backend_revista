4e58e175cd51316d597aabdb69d50098cc8a57eb
reham-shaban <reham.shaban2002@gmail.com>
2023-07-29 13:42:06 -0400

    notification on like, comment

--- revista/notifications/models.py
+++ revista/notifications/models.py
@@ -4,10 +4,12 @@ from django.conf import settings
 # Create your models here.
 class Notification(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='notifications/', blank=True, null=True)
+    profile_image = models.ImageField(upload_to='notifications/', blank=True, null=True)
     type = models.CharField(max_length=25)
     detail = models.TextField()
+    profile_id = models.IntegerField(blank=True, null=True)
+    post_id = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return f'{self.type} notification for {self.user.username}'
+        return f'{self.type} notification for {self.user}'
