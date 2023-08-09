eab7006ba20dd3327c1babde435cb973c9e44891
reham-shaban <reham.shaban2002@gmail.com>
2023-08-09 03:39:51 -0400

    edit url as flutter

--- revista/main/urls.py
+++ revista/main/urls.py
@@ -7,14 +7,14 @@ from . import views
 urlpatterns = [
     path('topics/', views.TopicListView.as_view()),
     path('topics-follow/', views.TopicFollowView.as_view()),
-    path('topics-unfollow/<int:pk>/', views.TopicUnFollowView.as_view()),
+    path('topics-unfollow/<int:pk>', views.TopicUnFollowView.as_view()),
     
     path('profile-list/', views.ProfileView.as_view()),
-    path('profile/<int:pk>/', views.SingleProfileView.as_view()),
+    path('profile/<int:pk>', views.SingleProfileView.as_view()),
     path('my-profile/', views.ProfileUpdateView.as_view()),
     
     path('follow/', views.FollowView.as_view()),
-    path('unfollow/<int:pk>/', views.UnFollowView.as_view()),
+    path('unfollow/<int:pk>', views.UnFollowView.as_view()),
     path('following-list/', views.FollowingListView.as_view()),
     path('followers-list/', views.FollowersListView.as_view()),
     
