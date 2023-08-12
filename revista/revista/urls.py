e1d8620ad173a5ac4854ab1d26f443b6a4869bea
reham-shaban <reham.shaban2002@gmail.com>
2023-08-12 06:27:07 -0400

    Update urls.py

--- revista/revista/urls.py
+++ revista/revista/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('report/', include('report.urls')),
     path('report-app/', include('report.api.urls')),
    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
